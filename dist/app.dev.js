8653fa719905eb4dec5251f989f2ddbbe6b53846
thecalmdevil <kampvid@gmail.com>
2022-07-06 20:09:49 -0400

    Add files via upload

---
+++ dist/app.dev.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+var mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+var menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+var header = document.querySelector('.header.container');
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('active');
+  mobile_menu.classList.toggle('active');
+});
+document.addEventListener('scroll', function () {
+  var scroll_position = window.scrollY;
+
+  if (scroll_position > 250) {
+    header.style.backgroundColor = '#29323c';
+  } else {
+    header.style.backgroundColor = 'transparent';
+  }
+});
+menu_item.forEach(function (item) {
+  item.addEventListener('click', function () {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+  });
+});
